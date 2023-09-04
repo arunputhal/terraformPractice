6aaa6a4010d213a75900837dcb5366e757a82804
arunputhal <arunputhal@gmail.com>
2023-09-04 11:57:38 +0530

    Adding Tag through Variable

---
+++ TerraformInputVariables/v8-2-InputVariablesMap_tag/v8-1-InputVariablesList/ec2instance.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "my-ec2-vm" {
+  ami                    = var.ec2_ami_id
+  availability_zone      = var.az
+  instance_type          = var.ec2_instance_type[2]
+  key_name               = "arunputhal"
+  count                  = var.ec2_instance_count
+  vpc_security_group_ids = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_web.id]
+  user_data              = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<h1> Welcome to My World</h1>"
+    EOF
+  tags = var.ec2_instance_tags
+}
